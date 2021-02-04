93c2a6c40278eb75092d0cfb3678125ef1249ad5
Filip <78152587+milovanovicf@users.noreply.github.com>
2021-02-04 16:43:03 +0100

    Update code.js

--- Profile portfolio/code.js
+++ Profile portfolio/code.js
@@ -12,3 +12,16 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.from(".welcome_animation", {scrollTrigger: {trigger:".welcome_animation", toggleActions:"restart none none none"},duration: 1.5, opacity: 0, y: -150,ease: "back", stagger: 0.3});
+gsap.from(".navbar_animation", {duration: 4, opacity: 0, x: -150, stagger: 0.3});
+
+
+gsap.from(".contact_animation", {scrollTrigger: {trigger:".contact_animation", toggleActions:"restart none none none"}, duration: 1, opacity: 0, y: -120,ease: "back", stagger: 0.3});
+
+gsap.from(".footer_animation_btn", {scrollTrigger: {trigger:"footer_animation_btn", toggleActions: "restart none none none"}, duration: 3, opacity: 0, y: -70,ease: "back", stagger: 0.3});
+gsap.from(".footer_animation_text", {scrollTrigger: {trigger:"footer_animation_text", toggleActions: "restart none none none"}, duration: 3, opacity: 0, x: -150, ease: "back", stagger: 0.3});
